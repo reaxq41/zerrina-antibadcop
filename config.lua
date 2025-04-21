3223f17c330128075b528418baea41c1fccb627d
zerrina22 <hurbaran756@gmail.com>
2025-04-21 19:29:34 +0300

    Add files via upload
    https://streamable.com/wit6up

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.Inventory = 'ox' -- 'qb'  'ox'
+
+Config.Weapons = {
+    `WEAPON_APPISTOL`,
+    `WEAPON_GLOCK`,
+    `WEAPON_COMBATPDW`,
+    `WEAPON_PUMPSHOTGUN_MK2`,
+}
+
+Config.Jobs = {
+    "police",
+    "bcso",
+    "lssd",
+    "sapr"
+}
+
+Config.Locale = {
+    notify = 'Polis silahını kullanamazsın'
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+shared_script '@fiveguard/ai_module_fg-obfuscated.lua'
+
+ 
+ 
+fx_version 'adamant'
+game 'gta5'
+client_script 'client.lua'
+shared_script 'config.lua'
